960c96251021650469aec1bf5e6fe488261da432
Gabriele-Cavallo <gabry.cava@gmail.com>
2024-09-17 14:14:37 +0200

    public / private / readonly

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,32 @@
 class Department {
-    name: string;
+    // private id: string,
+    // private name: string;
+    private employees: string[] = [];
 
-    constructor(n: string) {
-        this.name = n
+    constructor(private id: string, public name: string) {
+        // this.id = id;
+        // this.name = n
     }
 
     describe(this: Department) {
-        console.log('Department: ' + this.name);
+        console.log(`Department (${this.id}): ${this.name}`);
+    }
+
+    addEmploye(employee: string){
+        this.employees.push(employee);
+    }
+
+    printEmployeeInformation() {
+        console.log(this.employees.length);
+        console.log(this.employees);
     }
 };
 
-const accounting = new Department('Accounting');
-accounting.describe()
+const accounting = new Department('d1', 'Accounting');
+accounting.addEmploye('Max');
+accounting.addEmploye('Manu');
+accounting.describe();
+accounting.printEmployeeInformation();
 
-const accountingCopy = { name: 'DUMMY', describe: accounting.describe };
-accountingCopy.describe();
+// const accountingCopy = { name: 'DUMMY', describe: accounting.describe };
+// accountingCopy.describe();
